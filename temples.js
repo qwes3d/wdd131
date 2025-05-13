969ee4c0a8c450bd22a9419d9c0494325a98751d
qwes3d <uratation@gmail.com>
2025-05-13 19:37:10 +0000

    Update footer to include dynamic current year and last modified date in temples.html and add corresponding JavaScript functionality

---
+++ temples.js
@@ -0,0 +1,6 @@
+ // Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+    // Get the last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
